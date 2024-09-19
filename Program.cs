39d23d1c70547aa46429c6f9cefd4ce56994bd8f
ManuelSTP1201 <juanmasantacruz12@gmail.com>
2024-09-19 08:46:05 -0500

    Primer proyecto terminado

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿double sueldo, ganancia, nuevoS;
+
+Console.WriteLine("Ingrese su sueldo bruto: ");
+sueldo=double.Parse(Console.ReadLine());
+
+while (sueldo <= 600)
+{
+    ganancia=sueldo * 0.05;
+    nuevoS=sueldo+ganancia;
+}
